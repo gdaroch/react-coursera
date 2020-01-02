239965048a65d7e8411b7bf4c264e4223efead66
Gonzalo Cuadra <gonzalocuadra@protonmail.com>
2020-01-01 22:09:46 -0300

    Assignment 3

--- src/components/DishdetailComponent.jsx
+++ src/components/DishdetailComponent.jsx
@@ -1,6 +1,90 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button,
+         Modal, ModalHeader, ModalBody, ModalFooter, Row, Col, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
+const minLength = (len) => (val) => val && (val.length >= len);
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleSubmit(values){
+    alert('Comments: ' + JSON.stringify(values));
+  }
+
+  render(){
+    const { showModal, onToggleModal } = this.props;
+    return(
+      <Modal isOpen={showModal} toggle={onToggleModal}>
+        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+          <ModalHeader toggle={onToggleModal}>Submit Comment</ModalHeader>
+          <ModalBody>
+            <Row className="form-group">
+              <Label htmlFor="rating" md={2}>Rating</Label>
+                <Col md={10}>
+                    <Control.select
+                      model=".rating"
+                      name="rating"
+                      className="form-control"
+                    >
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </Control.select>
+                </Col>
+            </Row>
+            <Row className="form-group">
+              <Label htmlFor="author" md={2}>Your Name</Label>
+              <Col md={10}>
+                  <Control.text
+                    model=".author"
+                    id="author"
+                    name="author"
+                    placeholder="Your Name"
+                    className="form-control"
+                    validators={{
+                        minLength: minLength(3),
+                        maxLength: maxLength(15)
+                    }}
+                  />
+                  <Errors
+                      className="text-danger"
+                      model=".author"
+                      show="touched"
+                      messages={{
+                          minLength: 'Must be greater than 3 characters',
+                          maxLength: 'Must be 15 characters or less'
+                      }}
+                   />
+              </Col>
+            </Row>
+            <Row className="form-group">
+                <Label htmlFor="comment" md={2}>Comment</Label>
+                <Col md={10}>
+                    <Control.textarea
+                      model=".comment"
+                      id="comment"
+                      name="comment"
+                      rows="12"
+                      className="form-control"
+                    />
+                </Col>
+            </Row>
+          </ModalBody>
+          <ModalFooter>
+            <Button type="submit" color="primary">Submit</Button>
+          </ModalFooter>
+        </LocalForm>
+      </Modal>
+    );
+  }
+}
 
 function RenderDish({dish}){
   return(
@@ -16,27 +100,49 @@ function RenderDish({dish}){
   );
 }
 
-function RenderComments({comments}) {
-  if (comments != null) {
-    return (
-      <div className="col-12 col-md-5 m-1">
-        <h4>Comments</h4>
-        <ul className="list-unstyled">
-          {comments.map((comment) => {
-            return(
-              <li>
-                <p>{comment.comment}</p>
-                <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  } else {
-    return(
-      <div></div>
-    );
+class RenderComments extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    }
+    this.handleToggleModal = this.handleToggleModal.bind(this);
+  }
+
+  handleToggleModal() {
+    this.setState({ showModal: !this.state.showModal });
+  }
+
+  render() {
+    const {comments} = this.props;
+    if (comments != null) {
+      return (
+        <div className="col-12 col-md-5 m-1">
+          <h4>Comments</h4>
+          <ul className="list-unstyled">
+            {comments.map((comment) => {
+              return(
+                <li>
+                  <p>{comment.comment}</p>
+                  <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                </li>
+              );
+            })}
+          </ul>
+          <Button color="default" onClick={this.handleToggleModal}>
+            Submit Comment
+          </Button>
+          <CommentForm
+            onToggleModal={this.handleToggleModal}
+            showModal={this.state.showModal}
+          />
+        </div>
+      );
+    } else {
+      return(
+        <div></div>
+      );
+    }
   }
 }
 
