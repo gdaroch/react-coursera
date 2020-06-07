3aee1612c839ac99084ebce26ed3d6733e00ea8e
Gonzalo Cuadra <gonzalocuadra@protonmail.com>
2020-06-06 23:59:07 -0400

    Assignment 4

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -9,7 +9,14 @@ import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import About from './AboutComponent';
-import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
+import {
+  postComment,
+  postFeedback,
+  fetchDishes,
+  fetchComments,
+  fetchPromos,
+  fetchLeaders,
+} from "../redux/ActionCreators";
 import { actions } from 'react-redux-form';
 
 // Map the redux store's states into props to the components
@@ -23,8 +30,12 @@ const mapStateToProps = (state) => {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  postComment: (dishId, rating, author, comment) =>
-    dispatch(postComment(dishId, rating, author, comment)),
+  postComment: (dishId, rating, author, comment) => {
+    dispatch(postComment(dishId, rating, author, comment));
+  },
+  postFeedback: (firstname, lastname, telnum, email, agree, contactType, message) => {
+    dispatch(postFeedback(firstname, lastname, telnum, email, agree, contactType, message));
+  },
   fetchDishes: () => {
     dispatch(fetchDishes());
   },
@@ -33,6 +44,9 @@ const mapDispatchToProps = (dispatch) => ({
   },
   fetchPromos: () => {
     dispatch(fetchPromos());
+  },
+  fetchLeaders: () => {
+    dispatch(fetchLeaders());
   },
   resetFeedbackForm: () => {
     dispatch(actions.reset("feedback"));
@@ -45,6 +59,7 @@ class Main extends Component {
     this.props.fetchDishes();
     this.props.fetchComments();
     this.props.fetchPromos();
+    this.props.fetchLeaders();
   }
 
   render () {
@@ -57,7 +72,9 @@ class Main extends Component {
           promotion={this.props.promotions.promotions.filter((promotion) => promotion.featured)[0]}
           promosLoading={this.props.promotions.isLoading}
           promosErrMess={this.props.promotions.errMess}
-          leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+          leader={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
+          leadersLoading={this.props.leaders.isLoading}
+          leadersErrMess={this.props.leaders.errMess}
         />
       );
     }
@@ -79,7 +96,11 @@ class Main extends Component {
       <div className="App">
         <Header />
         <TransitionGroup>
-          <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+          <CSSTransition
+            key={this.props.location.key}
+            classNames="page"
+            timeout={300}
+          >
             <Switch>
               <Route path="/home" component={HomePage} />
               <Route
@@ -92,13 +113,22 @@ class Main extends Component {
                 exact
                 path="/contactus"
                 component={() => (
-                  <Contact resetFeedbackForm={this.props.resetFeedbackForm} />
+                  <Contact
+                    resetFeedbackForm={this.props.resetFeedbackForm}
+                    postFeedback={this.props.postFeedback}
+                  />
                 )}
               />
               <Route
                 exact
                 path="/aboutus"
-                component={() => <About leaders={this.props.leaders} />}
+                component={() => (
+                  <About
+                    leaders={this.props.leaders.leaders}
+                    isLoading={this.props.leaders.isLoading}
+                    errMess={this.props.leaders.errMess}
+                  />
+                )}
               />
               <Redirect to="/home" />
             </Switch>
--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -45,6 +45,62 @@ export const postComment =  (dishId, rating, author, comment) => (dispatch) => {
   });
 }
 
+export const postFeedback = (
+  firstname,
+  lastname,
+  telnum,
+  email,
+  agree,
+  contactType,
+  message
+) => (dispatch) => {
+
+  const newFeedback = {
+    firstname,
+    lastname,
+    telnum,
+    email,
+    agree,
+    contactType,
+    message,
+  };
+  newFeedback.date = new Date().toISOString();
+
+  return fetch(`${baseUrl}feedback`, {
+    method: "POST",
+    body: JSON.stringify(newFeedback),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "same-origin",
+  })
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          const error = new Error(
+            `Error ${response.status}: ${response.statusText}`
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      (error) => {
+        const errmess = new Error(error.message);
+        throw errmess;
+      }
+    )
+    .then((response) => response.json())
+    .then((jsonResponse) => {
+      alert(JSON.stringify(jsonResponse));
+    })
+    .catch((error) => {
+      console.log(`Post feedback ${error.message}`);
+      alert(`Your feedback cloud not be posted\nError: ${error.message}`);
+    });
+};
+
 export const fetchDishes = () => (dispatch) => {
   dispatch(dishesLoading(true));
 
@@ -145,4 +201,40 @@ export const promosFailed = (errmess) => ({
 export const addPromos = (promos) => ({
   type: ActionTypes.ADD_PROMOS,
   payload: promos,
+});
+
+export const fetchLeaders = () => (dispatch) => {
+  dispatch(leadersLoading(true));
+
+  return fetch(`${baseUrl}leaders`)
+    .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        const error = new Error(`Error ${response.status}: ${response.statusText}`);
+        error.response = response;
+        throw error;
+      }
+    },
+    error => {
+      const errmess = new Error(error.message);
+      throw errmess;
+    })
+    .then((response) => response.json())
+    .then((leaders) => dispatch(addLeaders(leaders)))
+    .catch(error => dispatch(leadersFailed(error.message)));
+};
+
+export const leadersLoading = () => ({
+  type: ActionTypes.LEADERS_LOADING,
+});
+
+export const leadersFailed = (errmess) => ({
+  type: ActionTypes.LEADERS_FAILED,
+  payload: errmess,
+});
+
+export const addLeaders = (leaders) => ({
+  type: ActionTypes.ADD_LEADERS,
+  payload: leaders,
 });
